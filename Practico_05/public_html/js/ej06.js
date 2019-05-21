1114360a1920302f8ddc13049f68b40ed8914302
romendez11 <30321859+romendez11@users.noreply.github.com>
2019-05-21 15:53:47 -0300

    arreglo

--- Practico_05/public_html/js/ej06.js
+++ Practico_05/public_html/js/ej06.js
@@ -1,3 +1,14 @@
-if texto < 0 // no es
 
-si no es y mostrar el texto
+$("#btnConvertir").click(convertirTexto);
+function convertirTexto(){
+    var texto = $("#texto").val();
+    var texto2 = $("#texto2").val();
+
+    var buscar = encontrarSubcadena(texto);
+    if(texto2 < 0){
+        $("#pVer").html("Es subcadena");
+    }else $("#pVer").html("No es subcadena");
+
+
+    $("#pVer").html(conversion);
+}
